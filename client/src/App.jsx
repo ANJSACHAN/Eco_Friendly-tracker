98fb3a524907ae98954b8c481a6b53cf2b6f2796
Whitecoder <86569824+abhishek20111@users.noreply.github.com>
2024-06-15 12:06:06 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import ErrorPage from './component/ErrorPage'
+import Home from './component/Home'
+import { ToastContainer } from 'react-toastify';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      App
+      <AppRoutes/>
     </div>
   )
+  
+function AppRoutes() {
+  return (<>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="*" element={<ErrorPage/>} />
+    </Routes>
+
+    <ToastContainer />
+  </>
+  )
+}
 }
 
 export default App
